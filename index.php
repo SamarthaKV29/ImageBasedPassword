e636bb33f17502493f8236828effa5c07e77b3d2
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-17 19:24:13 -0400

    UPD

--- index.php
+++ index.php
@@ -40,7 +40,25 @@
                         </div>
                     
                     </div>
-                    <div class="col">Login</div>            
+                    <div class="col">
+                        <div class="panel panel-primary">
+                            <div class="panel panel-heading">
+                            LOGIN
+                            </div>
+                            <div class="panel panel-body">
+                                <form >
+                                    <div class="form-group">
+                                        <label>EmailID:</label>
+                                        <input class="form-control" name="userid" required type="email"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password:</label>
+                                        <input class="form-control" required name="pass" type="password"/>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>            
                 
                 
                 
