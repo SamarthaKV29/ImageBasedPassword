c0f2eaf1522c3cceed40bdc0123f580562414b83
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-17 19:17:11 -0400

    UPD

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,11 @@
 <?php 
 
 
-// function pg_connection_string_from_database_url() {
-//     extract(parse_url($_ENV["DATABASE_URL"]));
-//     return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-//   }
-
-// echo "".pg_connection_string_from_database_url();
-
+function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+  }
+ 
 
 
 function getDBC(){
@@ -16,11 +14,8 @@ function getDBC(){
     // $password="6171a7ebf6524897ed492ac8c786242030f8997068db778a71954365002a7578";
     // $host="ec2-54-243-28-109.compute-1.amazonaws.com";
     // $dbname="d52j9hr7kohal6";
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    echo $_ENV["DATABASE_URL"];
-    echo 'pgsql:host='.$host.';dbname='.$dbname." ". $user." ". $pass;
     try{
-        $dbh = new PDO('pgsql:host='.$host.';dbname='.$dbname, $user, $pass);
+        $dbh = pg_connect(pg_connection_string_from_database_url());
     }
     catch(Exception $e){
         echo "Unable to connect to database. Please try again.";
