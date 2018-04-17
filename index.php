18bd3fc33b9dc4a5bdb3617a44fe396532964f36
SamarthaKV29 <samarthakv29@gmail.com>
2018-04-17 19:18:45 -0400

    UPD

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
 				}
 				?>
                 <div class="row">
-                    <div class="col-md-2">Menu</div>
-                    <div class="col-md-4">
+                    <div class="col">Menu</div>
+                    <div class="col-md-6">
                         <div class="panel panel-default">
                             <div class="panel panel-heading">
                                 <h3>Welcome to IBP</h3>
@@ -40,7 +40,7 @@
                         </div>
                     
                     </div>
-                    <div class="col-md-2">Login</div>            
+                    <div class="col">Login</div>            
                 
                 
                 
